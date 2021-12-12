3456bea1b21e3e099cece8df33eeaeeef3ce8631
Amit Sharma <95172045+amitsharma-codes@users.noreply.github.com>
2021-12-12 23:20:06 +0530

    Add files via upload

---
+++ Typecast/typecasting.c
@@ -0,0 +1,10 @@
+
+/* Type "man operator" command in the terminal for more details on typecasting */
+
+#include<stdio.h>
+void main()
+{
+	int a = -4;
+	unsigned int b=3;
+	printf("%u , %d \n",a,b);
+}
