d047fbabe65eaedc723017c17f912aa5fbcaeb2f
Amit Sharma <95172045+amitsharma-codes@users.noreply.github.com>
2021-12-12 23:05:08 +0530

    Add files via upload

---
+++ Compilation Stages/prog.s
@@ -0,0 +1,41 @@
+	.arch armv6
+	.eabi_attribute 28, 1
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 2
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 1
+	.eabi_attribute 18, 4
+	.file	"prog.c"
+	.text
+	.section	.rodata
+	.align	2
+.LC0:
+	.ascii	"Hello World\000"
+	.text
+	.align	2
+	.global	main
+	.arch armv6
+	.syntax unified
+	.arm
+	.fpu vfp
+	.type	main, %function
+main:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 1, uses_anonymous_args = 0
+	push	{fp, lr}
+	add	fp, sp, #4
+	ldr	r0, .L2
+	bl	printf
+	nop
+	pop	{fp, pc}
+.L3:
+	.align	2
+.L2:
+	.word	.LC0
+	.size	main, .-main
+	.ident	"GCC: (Raspbian 8.3.0-6+rpi1) 8.3.0"
+	.section	.note.GNU-stack,"",%progbits
